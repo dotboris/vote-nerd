bd4d0dac274b5c93faeaca339c9f018f2b1e9c7a
Boris Bera <bbera@pmctire.com>
2017-05-19 19:33:10 -0400

    Generate project with mix new

---
+++ lib/vote_nerd.ex
@@ -0,0 +1,18 @@
+defmodule VoteNerd do
+  @moduledoc """
+  Documentation for VoteNerd.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VoteNerd.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vote_nerd_test.exs
@@ -0,0 +1,8 @@
+defmodule VoteNerdTest do
+  use ExUnit.Case
+  doctest VoteNerd
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end

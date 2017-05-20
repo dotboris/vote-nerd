18686fadc0e055eb55d6ade43ce76564f9ea7e3e
Boris Bera <bbera@pmctire.com>
2017-05-19 21:01:52 -0400

    Add coveralls dep

--- mix.exs
+++ mix.exs
@@ -2,12 +2,21 @@ defmodule VoteNerd.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :vote_nerd,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :vote_nerd,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      deps: deps()
+    ]
   end
 
   def application do
@@ -28,7 +37,8 @@ defmodule VoteNerd.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:nadia, "~> 0.4.2"}
+      {:nadia, "~> 0.4.2"},
+      {:excoveralls, "~> 0.6", only: :test}
     ]
   end
 end

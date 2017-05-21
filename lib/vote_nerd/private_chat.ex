5c7d3d6369b27f12c395544f98935228e70dce0b
Boris Bera <bbera@pmctire.com>
2017-05-21 17:54:39 -0400

    Add basic poll building
    For some reason the buttons require a url. Need to figure out WTF is
    going on here.

--- lib/vote_nerd/private_chat.ex
+++ lib/vote_nerd/private_chat.ex
@@ -18,7 +18,7 @@ defmodule VoteNerd.PrivateChat do
   def init(chat_id) do
     {:ok, %{
       chat_id: chat_id,
-      title: "",
+      title: nil,
       options: [],
       building: false
     }}
@@ -35,6 +35,60 @@ defmodule VoteNerd.PrivateChat do
     Nadia.send_message(chat_id, @help_text)
 
     state
+  end
+
+  defp handle_text("/start", %{chat_id: chat_id, building: true} = state) do
+    Nadia.send_message(chat_id,
+      "We're already creating a poll. Use /cancel to reset everything."
+    )
+
+    state
+  end
+
+  defp handle_text("/start", %{chat_id: chat_id, building: false} = state) do
+    Nadia.send_message(chat_id,
+      "OK. Let's get started. What would you like to name your poll?"
+    )
+
+    %{state | building: true}
+  end
+
+  defp handle_text("/cancel", %{chat_id: chat_id} = state) do
+    Nadia.send_message(chat_id, "OK. I reset everything.")
+
+    %{state | title: nil, options: [], building: false}
+  end
+
+  defp handle_text("/done", %{chat_id: chat_id, building: true, title: title, options: options} = state) do
+    buttons = options
+    |> Enum.map(fn o -> %Nadia.Model.InlineKeyboardButton{
+      text: o,
+      url: "http://perdu.com" # TODO: figure out why :callback_data doesn't work
+    } end)
+    |> Enum.map(fn x -> [x] end)
+
+    Nadia.send_message(
+      chat_id,
+      "*#{title}*",
+      parse_mode: "Markdown",
+      reply_markup: %Nadia.Model.InlineKeyboardMarkup{
+        inline_keyboard: buttons
+      }
+    )
+
+    %{state | title: nil, options: [], building: false}
+  end
+
+  defp handle_text(title, %{chat_id: chat_id, building: true, title: nil} = state) do
+    Nadia.send_message(chat_id, "OK. Now start typing some options.")
+
+    %{state | title: title}
+  end
+
+  defp handle_text(option, %{chat_id: chat_id, building: true, options: options, title: title} = state) when is_binary(title) do
+    Nadia.send_message(chat_id, "OK. Type in another option. If you're done use /done")
+
+    %{state | options: options ++ [option]}
   end
 
   defp handle_text(_text, %{chat_id: chat_id} = state) do
--- lib/vote_nerd/router.ex
+++ lib/vote_nerd/router.ex
@@ -2,7 +2,6 @@ defmodule VoteNerd.Router do
   alias VoteNerd.{Registry, PrivateChat}
 
   def dispatch(%{message: %{chat: %{type: "private", id: chat_id}}} = update) do
-    IO.puts("Matched private chat #{chat_id}")
     Registry.chat(Registry, chat_id)
     |> PrivateChat.update(update)
   end

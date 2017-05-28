099d18480be7cfbf4ddd4c5baa7c79fc6b4a0e50
Boris Bera <bera.boris@gmail.com>
2017-05-28 09:29:46 -0400

    Added voting to poll

--- lib/vote_nerd/poll.ex
+++ lib/vote_nerd/poll.ex
@@ -21,4 +21,64 @@ defmodule VoteNerd.Poll do
     poll
     |> Map.update!(:options, &([o | &1]))
   end
+
+  @doc """
+  Starts the `poll` allowing people to vote
+
+  ## Examples
+
+  iex> %VoteNerd.Poll{options: ["b", "a"]}
+  ...> |> VoteNerd.Poll.start
+  %VoteNerd.Poll{
+    options: ["b", "a"],
+    votes: %{0 => MapSet.new, 1 => MapSet.new}
+  }
+  """
+  def start(%{options: os} = poll) do
+    votes = for i <- 0..(length(os) - 1),
+      into: %{},
+      do: {i, MapSet.new}
+
+    Map.put(poll, :votes, votes)
+  end
+
+  @doc """
+  Votes as `voter` for the option with the given `index`.
+
+  Note that this acs as a toggle. Voting for something that you've already
+  voted for removes your vote.
+
+  ## Examples
+
+  iex> %VoteNerd.Poll{votes: %{0 => MapSet.new}}
+  ...> |> VoteNerd.Poll.vote("bob", 0)
+  %VoteNerd.Poll{votes: %{0 => MapSet.new(["bob"])}}
+
+  iex> %VoteNerd.Poll{votes: %{0 => MapSet.new}}
+  ...> |> VoteNerd.Poll.vote("bob", 0)
+  ...> |> VoteNerd.Poll.vote("bob", 0)
+  %VoteNerd.Poll{votes: %{0 => MapSet.new()}}
+
+  iex> %VoteNerd.Poll{votes: %{0 => MapSet.new, 1 => MapSet.new}}
+  ...> |> VoteNerd.Poll.vote("bob", 0)
+  ...> |> VoteNerd.Poll.vote("bob", 1)
+  %VoteNerd.Poll{votes: %{0 => MapSet.new(["bob"]), 1 => MapSet.new(["bob"])}}
+
+  iex> %VoteNerd.Poll{votes: %{0 => MapSet.new, 1 => MapSet.new}}
+  ...> |> VoteNerd.Poll.vote("bob", 0)
+  ...> |> VoteNerd.Poll.vote("bob", 1)
+  ...> |> VoteNerd.Poll.vote("bob", 1)
+  %VoteNerd.Poll{votes: %{0 => MapSet.new(["bob"]), 1 => MapSet.new}}
+  """
+  def vote(%{votes: vs} = poll, voter, index) do
+    vs = Map.update!(vs, index, fn v ->
+      if MapSet.member?(v, voter) do
+        MapSet.delete(v, voter)
+      else
+        MapSet.put(v, voter)
+      end
+    end)
+
+    Map.put(poll, :votes, vs)
+  end
 end
